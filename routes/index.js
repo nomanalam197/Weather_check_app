5b3137c0a7be5a57e8eac42ba6358c333a0be4cf
MD NOMAN ALAM <nomanalam197@gmail.com>
2022-11-07 23:46:03 +0530

    updated

--- routes/index.js
+++ routes/index.js
@@ -23,22 +23,30 @@ router.post('/',function(req,res){
   https.get(url,function(response){
 
     response.on("data",function(data){
-      
       const weatherdata=  JSON.parse(data);
-
+      // res.send(weatherdata);
       var temp =  weatherdata.main.temp;
       var description =  weatherdata.weather[0].description;
       var icon =  weatherdata.weather[0].icon;
-      var imageurl = "http://openweathermap.org/img/wn/"+icon +"@2x.png"
+      var imageurl = "http://openweathermap.org/img/wn/"+icon +"@2x.png";
+      var temp_max = weatherdata.main.temp_max;
+      var temp_min = weatherdata.main.temp_min;
+      var pressure = weatherdata.main.pressure;
+      var humidity = weatherdata.main.humidity;
+      var visibility =weatherdata.visibility;
+      var latitude = weatherdata.coord.lat;
+      var longitude = weatherdata.coord.lon;
+      var wind_speed = weatherdata.wind.speed;
+      var sunrise = weatherdata.sys.sunrise;
+      var sunset = weatherdata.sys.sunset;
       
-      
-      res.render('display',{place:place, temp:temp, description:description, imageurl:imageurl})
+      res.render('display',{place:place, temp:temp, description:description, imageurl:imageurl, unit:unit, temp_max:temp_max, temp_min:temp_min, pressure:pressure, humidity:humidity, visibility:visibility, latitude:latitude, longitude:longitude, wind_speed:wind_speed, sunrise:sunrise, sunset:sunset})
     })
   })
 })
 
-router.get('/Data',function(req,res){
-  
+router.get('/display',function(req,res){
+  res.render('display')
 })
 
 module.exports = router;
